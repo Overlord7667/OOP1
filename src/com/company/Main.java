f63d780cbdaa4fa030cf38d9cf6dfa43637c5a19
Overlord7667 <aspiaro@gmail.com>
2021-11-08 01:11:33 +0300

    OOP2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Person vitya = new Person();
-        vitya.height = 120;
+        Person vitya = new Person(120, "Vitya ");
         vitya.say("George");
-        System.out.println(vitya.height);
+        System.out.println(vitya.name + "Is height is " + vitya.height);
         Person vlad = new Person();
-        System.out.print(vlad.height);
+        System.out.println(vlad.height);
+
+        Person dima = new Person("Dima");
+        System.out.println(dima.name);
+        System.out.println("");
+
+        Student freshman = new Student(76, "Viktor", 2);
+        freshman.tell();
+        Student fresh = new Student(280, "Vlad", 4);
+        fresh.tell();
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -2,8 +2,18 @@ package com.company;
 
 public class Person {
     public int height = 180;
+    public  String name = "Default";
 
     public void say(String name){
-        System.out.println("Hello, " + name);
+        System.out.println("Hello " + name);
+    }
+    public Person(){}
+
+    public Person(int h, String n) {
+        height = h;
+        name = n;
+    }
+    public Person(String n){
+        name = n;
     }
 }
